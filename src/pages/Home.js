0663b4d50c29b108c2cee093b14430196bc031ac
Saswat <mohantysaswat154@gmail.com>
2026-01-18 20:38:11 +0530

    using <Link> from react-router for navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Homepage() {
   return (
     <>
       <h1>Welcome to the Homepage</h1>
       <p>
-        Go to <a href="/products">the list of products</a>
+        Go to <Link to="/products">the list of products</Link>
       </p>
     </>
   );
