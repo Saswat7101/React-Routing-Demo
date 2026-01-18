e332e2162e173700de46c84951ec17340a54e6d1
Saswat <mohantysaswat154@gmail.com>
2026-01-18 20:32:29 +0530

    creating routes with JSX code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import Homepage from "./pages/Home";
 import ProducstPage from "./pages/Products";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Homepage /> },
-  { path: "/products", element: <ProducstPage /> },
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Homepage />} />
+    <Route path="/products" element={<ProducstPage />} />
+  </Route>,
+);
+
+// const router = createBrowserRouter([
+//   { path: "/", element: <Homepage /> },
+//   { path: "/products", element: <ProducstPage /> },
+// ]);
+
+const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
